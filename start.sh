3c79451e86e2ad6e703ca7d04f6be2069a01a10a
colossus9 <colossus9@hpe.com>
2016-03-20 05:01:42 +0000

    Initial commit of working rafiki hubot

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# pkill -f nono
+
+HUBOT_HOME_DIR=./
+HUBOT_BIN='bin/hubot'
+HUBOT_LOGS_DIR=~/logs
+
+cd ${HUBOT_HOME_DIR}
+if [ "$(pgrep node)" != "" ]
+then
+    echo "Killing previous process... ($(pgrep node))"
+    pkill node
+fi
+
+mkdir -p ${HUBOT_LOGS_DIR}
+
+if [ ! -f ./hubot.env ]; then
+    echo "hubot.env file not found!"
+    exit 1
+fi
+
+source ./hubot.env
+
+${HUBOT_HOME_DIR}${HUBOT_BIN} --name ${HUBOT_NAME} --adapter ${ADAPTER} >${HUBOT_LOGS_DIR}/${HUBOT_NAME}.log 2>&1 &
+
+sleep 2
+echo NONO PID:
+pgrep node
+tail -n 20 ${HUBOT_LOGS_DIR}/${HUBOT_NAME}.log
+
