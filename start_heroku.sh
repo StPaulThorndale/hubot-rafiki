bfad8f06b481563e79a2eb71621161d921c8e95b
James Garcia <jrgarcia786@gmail.com>
2017-09-22 22:23:26 -0500

    Heroku start

--- start_heroku.sh
+++ start_heroku.sh
@@ -15,14 +15,12 @@ fi
 
 mkdir -p ${HUBOT_LOGS_DIR}
 
-# if [ ! -f ./hubot.env ]; then
-#    echo "hubot.env file not found!"
-#    exit 1
-# fi
+if [ ! -f ./hubot.heroku.env ]; then
+   echo "hubot.heroku.env file not found!"
+   exit 1
+fi
 
-# source ./hubot.env
-
-echo "TEST_VAR is $TEST_VAR"
+source ./hubot.heroku.env
 
 ${HUBOT_HOME_DIR}${HUBOT_BIN} --name ${HUBOT_NAME} --adapter ${ADAPTER} >${HUBOT_LOGS_DIR}/${HUBOT_NAME}.log 2>&1 &
 
